53bf90e684262abd6902b3b7cad7a0b841d688a3
MuhammadUmair151601 <157686720+MuhammadUmair151601@users.noreply.github.com>
2025-10-14 10:26:41 +0500

    text input areas done

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,16 +3,16 @@ import { FaUser, FaLock } from "react-icons/fa";
 
 const Login = () => {
   return (
-    <div className='wrapper'>
+    <div className='wrapper bg-purple-500 h-100 w-100'>
       <form action="">
-        <h1 className='text-3xl font-bold'>Login</h1>
-        <div>
-          <input type="text" placeholder='Username' required />
-          <FaUser />
+        <h1 className='text-3xl font-bold flex justify-center mt-5 text-white'>Login</h1>
+        <div className='relative'>
+          <input className='bg-sky-300 w-full py-3 px-10 rounded-[30px] text-white text-xl mt-4 border border-gray-400 bg-transparent focus:outline-none focus:ring-0' type="text" placeholder='Username' required />
+          <FaUser className='absolute top-8 left-93 text-white' />
         </div>
-        <div>
-          <input type="password" placeholder='Password' required />
-          <FaLock />
+        <div className='relative'>
+          <input className='bg-sky-300 w-full py-3 px-10 rounded-[30px] text-white text-xl mt-4 border border-gray-400 bg-transparent focus:outline-none focus:ring-0' type="password" placeholder='Password' required />
+          <FaLock className='absolute top-8 left-93 text-white' />
           </div>
           <div>
             <label><input type="checkbox"/>Remember me</label>
