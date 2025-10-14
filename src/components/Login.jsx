9278ff25829f9f0bf481ae1a99ca97a14d9ca430
MuhammadUmair151601 <157686720+MuhammadUmair151601@users.noreply.github.com>
2025-10-14 11:20:01 +0500

    bg pic added

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,30 +1,53 @@
-import React from 'react'
+import React from "react";
 import { FaUser, FaLock } from "react-icons/fa";
 
 const Login = () => {
   return (
-    <div className='wrapper bg-purple-500 h-100 w-100'>
-      <form action="">
-        <h1 className='text-3xl font-bold flex justify-center mt-5 text-white'>Login</h1>
-        <div className='relative'>
-          <input className='bg-sky-300 w-full py-3 px-10 rounded-[30px] text-white text-xl mt-4 border border-gray-400 bg-transparent focus:outline-none focus:ring-0' type="text" placeholder='Username' required />
-          <FaUser className='absolute top-8 left-93 text-white' />
+    <div className="wrapper  h-100 w-110 relative bg-purple-300 rounded-[30px]">
+      <form action="" className="ml-10 mr-10">
+        <h1 className="text-4xl font-bold flex justify-center mt-5 text-white">
+          Login
+        </h1>
+        <div className="relative">
+          <input
+            className="bg-sky-300 w-full py-3 px-10 rounded-[30px] text-white text-xl mt-4 border border-gray-400 bg-transparent focus:outline-none focus:ring-0"
+            type="text"
+            placeholder="Username"
+            required
+          />
+          <FaUser className="absolute top-8 left-82 text-white" />
         </div>
-        <div className='relative'>
-          <input className='bg-sky-300 w-full py-3 px-10 rounded-[30px] text-white text-xl mt-4 border border-gray-400 bg-transparent focus:outline-none focus:ring-0' type="password" placeholder='Password' required />
-          <FaLock className='absolute top-8 left-93 text-white' />
-          </div>
-          <div>
-            <label><input type="checkbox"/>Remember me</label>
-            <a href="#">forgot password?</a>
-          </div>
-          <button type='submit'>Login</button>
-          <div>
-            <p>Don't have an account?</p> <a href="#">Register</a>
-          </div>
+        <div className="relative">
+          <input
+            className="bg-sky-300 w-full py-3 px-10 rounded-[30px] text-white text-xl mt-4 border border-gray-400 bg-transparent focus:outline-none focus:ring-0"
+            type="password"
+            placeholder="Password"
+            required
+          />
+          <FaLock className="absolute top-8 left-82 text-white" />
+        </div>
+        <div className="flex justify-between text-white mt-5 px-10">
+          <label>
+            <input type="checkbox" />
+            Remember me
+          </label>
+          <a href="#">forgot password?</a>
+        </div>
+        <button
+          className="mt-5  text-white px-40 ml-3 rounded-[20px] text-xl font-bold border border-gray-400 bg-transparent focus:outline-none focus:ring-0 cursor-pointer"
+          type="submit"
+        >
+          Login
+        </button>
+        <div className="flex justify-center gap-4 mt-5 text-white ">
+          <p className="cursor-pointer">Don't have an account?</p>{" "}
+          <a className="font-bold" href="#">
+            Register
+          </a>
+        </div>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
