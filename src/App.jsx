f1e98b54f09ab95cad123575f51ed55051d8ddc6
MuhammadUmair151601 <157686720+MuhammadUmair151601@users.noreply.github.com>
2025-10-17 09:45:43 +0500

    background added and some text and coloring adjusted

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Login from './components/login'
-
+import BgImage from './assets/plan-webpage-layout-design.png'
 
 const App = () => {
   return (
-    <div className='flex justify-center items-center h-screen'>
+    <div className="flex justify-center items-center h-screen bg-cover bg-center" 
+    style={{ backgroundImage: `url(${BgImage})` }}
+    >
       <Login/>
     </div>
   )
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,44 +3,44 @@ import { FaUser, FaLock } from "react-icons/fa";
 
 const Login = () => {
   return (
-    <div className="wrapper  h-100 w-110 relative bg-purple-300 rounded-[30px]">
-      <form action="" className="ml-10 mr-10">
-        <h1 className="text-4xl font-bold flex justify-center mt-5 text-white">
-          Login
+    <div className="h-100 w-110 rounded-[30px] bg-yellow-600">
+      <form action="" className="ml-10 mr-10 bg-yellow-600 font-bold">
+        <h1 className="text-4xl font-bold flex justify-center mt-5 text-black">
+          Welcome Back!
         </h1>
         <div className="relative">
           <input
-            className="bg-sky-300 w-full py-3 px-10 rounded-[30px] text-white text-xl mt-4 border border-gray-400 bg-transparent focus:outline-none focus:ring-0"
+            className="w-full py-3 px-10 rounded-[30px] text-black text-xl mt-4 border border-gray-400 focus:outline-none focus:ring-0"
             type="text"
             placeholder="Username"
             required
           />
-          <FaUser className="absolute top-8 left-82 text-white" />
+          <FaUser className="absolute top-8 left-82 text-black" />
         </div>
         <div className="relative">
           <input
-            className="bg-sky-300 w-full py-3 px-10 rounded-[30px] text-white text-xl mt-4 border border-gray-400 bg-transparent focus:outline-none focus:ring-0"
+            className="w-full py-3 px-10 rounded-[30px] text-black text-xl mt-4 border border-gray-400 focus:outline-none focus:ring-0"
             type="password"
             placeholder="Password"
             required
           />
-          <FaLock className="absolute top-8 left-82 text-white" />
+          <FaLock className="absolute top-8 left-82 text-black" />
         </div>
         <div className="flex justify-between text-white mt-5 px-10">
-          <label>
+          <label className="text-black">
             <input type="checkbox" />
             Remember me
           </label>
-          <a href="#">forgot password?</a>
+          <a className="text-black" href="#">forgot password?</a>
         </div>
         <button
-          className="mt-5  text-white px-40 ml-3 rounded-[20px] text-xl font-bold border border-gray-400 bg-transparent focus:outline-none focus:ring-0 cursor-pointer"
+          className="mt-5 text-black px-38 rounded-[20px] text-xl font-bold border border-gray-400 focus:outline-none focus:ring-0 cursor-pointer hover:bg-yellow-800"
           type="submit"
         >
           Login
         </button>
-        <div className="flex justify-center gap-4 mt-5 text-white ">
-          <p className="cursor-pointer">Don't have an account?</p>{" "}
+        <div className="flex justify-center gap-4 mt-5 text-black ">
+          <p className="cursor-pointer">Don't have an account?</p>
           <a className="font-bold" href="#">
             Register
           </a>
